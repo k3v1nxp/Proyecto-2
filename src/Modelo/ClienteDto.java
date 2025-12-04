ab1cf048832e5bbc11d38da83fdac8d2a052843e
Salazar <jdrsalazar1025@gmail.com>
2025-12-04 10:44:35 -0600

    clases Dto
    se crean las clases Dto de entrenador y cliente junto con el contenido respectivo, abierto a mejoras si es necesario

--- src/Modelo/ClienteDto.java
+++ src/Modelo/ClienteDto.java
@@ -4,10 +4,46 @@
  */
 package Modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author UTN
  */
 public class ClienteDto {
-    
+   private final int id; 
+   private final String nombre;
+   private final TipoMembresia menbresia;
+   private final Date fecha_nacimiento;
+   private final int Numero;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TipoMembresia getMenbresia() {
+        return menbresia;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public ClienteDto(int id, String nombre, TipoMembresia menbresia, Date fecha_nacimiento, int Numero) {
+        this.id = id;
+        this.nombre = nombre;
+        this.menbresia = menbresia;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.Numero = Numero;
+    }
+   
+   
 }
--- src/Modelo/EntrenadorDto.java
+++ src/Modelo/EntrenadorDto.java
@@ -9,5 +9,22 @@ package Modelo;
  * @author UTN
  */
 public class EntrenadorDto {
+    private final int id;
+   private final String nombre;
+   private final String contacto;
+   private final TiposEspecialidades especialidad;
+
+    public int getId() { return id;}
+    public String getNombre() {return nombre;}
+    public String getContacto() {return contacto;}
+    public TiposEspecialidades getEspecialidad() {  return especialidad;}
+
+    public EntrenadorDto(int id, String nombre, String contacto, TiposEspecialidades especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.especialidad = especialidad;
+    }
+    
     
 }

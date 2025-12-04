af4643e871645508be916c1b9a32e331fb5014d8
Salazar <jdrsalazar1025@gmail.com>
2025-12-04 10:46:17 -0600

    Clase ENTRENADOR
    se le agrego contenido estándar , abierto a mejoras

--- src/Modelo/Entrenador.java
+++ src/Modelo/Entrenador.java
@@ -9,5 +9,28 @@ package Modelo;
  * @author UTN
  */
 public class Entrenador {
-    
+   private int id;
+   private String nombre;
+   private String contacto;
+   private TiposEspecialidades especialidad;
+
+    public int getId() { return id;}
+    public String getNombre() {return nombre;}
+    public String getContacto() {return contacto;}
+    public TiposEspecialidades getEspecialidad() {  return especialidad;}
+
+    public void setId(int id) {this.id = id;}
+    public void setNombre(String nombre) { this.nombre = nombre;}
+    public void setContacto(String contacto) {this.contacto = contacto;}
+    public void setEspecialidad(TiposEspecialidades especialidad) {  this.especialidad = especialidad;}
+      
+
+    public Entrenador(int id, String nombre, String contacto, TiposEspecialidades especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.especialidad = especialidad;
+    }
+   
+   
 }

50951753e8ba35759c420ede8c894a9b92d1c4bd
Salazar <jdrsalazar1025@gmail.com>
2025-12-08 10:11:14 -0600

    clase connection
    falta analisis y agregar la libreria correspondiente

---
+++ src/Modelo/ConexionBD.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author UTN
+ */
+public class ConexionBD {
+//    private static ConexionBD instancia;
+//    private Connection conexion;
+    
+//    private final String URL = "jdbc:mariadb://localhost:3306/GimnasioApp";
+//    private final String USUARIO = "tu_usuario";
+////    private final String PASSWORD = "tu_contraseña";
+    
+//     private ConexionBD() {
+//        try {
+//            conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
